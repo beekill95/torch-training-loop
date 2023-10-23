3fcdfde171d74e1cb4397de187d126b1d6befddf
beekill <nguyenmbquan95@gmail.com>
2023-10-23 14:05:58 +0700

    s/training_loop/torch_training_loop when fetching version string.

--- src/training_loop/__init__.py
+++ src/training_loop/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import version, PackageNotFoundError
 from .training_loops import SimpleTrainingLoop, TrainingLoop
 
 try:
-    __version__ = version('training_loop')
+    __version__ = version('torch_training_loop')
 except PackageNotFoundError:
     # If the package is not installed, don't add __version__
     pass
