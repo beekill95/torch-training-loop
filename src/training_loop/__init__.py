a8276d77ddfe9eb038f112d09f019ee5b5a3164e
Bao Quan <nguyenmbquan95@gmail.com>
2023-10-23 14:09:48 +0700

    Implement package versioning based on Git tags (#3)
    * Use dynamic versioning plugin for poetry.
    * Rename project to torch-training-loop in pyproject.toml, add package metadata.

--- src/training_loop/__init__.py
+++ src/training_loop/__init__.py
@@ -1,1 +1,9 @@
+from importlib.metadata import version, PackageNotFoundError
+
 from .training_loops import SimpleTrainingLoop, TrainingLoop
+
+try:
+    __version__ = version('torch_training_loop')
+except PackageNotFoundError:
+    # If the package is not installed, don't add __version__
+    pass
